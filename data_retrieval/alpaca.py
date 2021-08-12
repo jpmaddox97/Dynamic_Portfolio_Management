2f8108b7e2ecd97c3ca4e4d4a032e50400e39bf2
Joshua Maddox <jpmaddox97@gmail.com>
2021-08-11 20:31:17 -0400

    Made function to get user risk tolerance

--- data_retrieval/alpaca.py
+++ data_retrieval/alpaca.py
@@ -1,14 +1,28 @@
 import os
 import pandas as pd
-import json
-import requests
 from dotenv import load_dotenv
 import alpaca_trade_api as trade_api
 from datetime import date
 
 class Alpaca:
+    """
+    A class that accepts inputs for tickers, the api key, 
+    it's secret key, a time frame as well as the amount of 
+    years of historical data requested. The object then 
+    returns a cleaned dataframe of just close prices and volume.
 
+    Inputs:
+        ticker_list = list of tickers that we want data for 
+        alpaca_api, alpaca_secret_key = name of alpaca api saved to env variable
+        timeframe = expressed as ("1(D,W,M,H,S)")
+        endpoint_years = accepts an int amount of years
+
+    Output:
+        Cleaned dataframe that stores the close and volume values for each stock
+    """
     def __init__(self, ticker_list, alpaca_api, alpaca_secret_key, timeframe, endpoint_years):
+        # Init class variables and set them equal to inputs
+
         self.ticker_list = ticker_list
         self.alpaca_api = alpaca_api
         self.alpaca_secret_key = alpaca_secret_key
@@ -16,31 +30,42 @@ class Alpaca:
         self.endpoint_years = endpoint_years
 
     def load_variables(self):
+        # Load environment variables to make api calls
+
         load_dotenv()
         self.alpaca_api = os.getenv(self.alpaca_api)
         self.alpaca_secret_key = os.getenv(self.alpaca_secret_key)
 
 
     def create_api_call(self):
+        # Set up alpaca api call
+
         self.alpaca = trade_api.REST(
         self.alpaca_api,
         self.alpaca_secret_key,
         api_version="v2")
+
         return self.alpaca
 
     def clean_tickers(self):
+        # Take the list of tickers ipnut by the user and properly format for api call
+
         self.tickers = []
         for ticker in self.ticker_list:
             ticker = ticker.upper()
             self.tickers.append(ticker)
 
-        # print(self.tickers)
-
         return self.tickers
 
     def get_dataframe(self):
+        # Make the api call
+
+        # Using the date module and pandas Timedelta we get the current day as the start
+        # And using the input from the user we go back x amount of years
         end_date = date.today() - pd.Timedelta(days=1)
         start_date = (end_date - pd.Timedelta(weeks=52*(self.endpoint_years)))
+
+        # Make alpaca api call
         self.df_portfolio = self.alpaca.get_barset(
         self.tickers,
         self.timeframe,
@@ -48,18 +73,17 @@ class Alpaca:
         end = pd.Timestamp(end_date, tz="America/New_York").isoformat(),
         limit=1000
         ).df
-        
-        # print(type(self.df_portfolio))
-        # print(self.df_portfolio)
 
         return self.df_portfolio
 
     def clean_dataframe(self):
+        # Drop NaN values and set index to a pure date (yyyy-mm-dd)
         self.df_portfolio = self.df_portfolio.dropna()
         self.df_portfolio.index = self.df_portfolio.index.date
         clean_ticker_df = []
         columns = []
 
+        # Set column names and parse dataframe down to just close and volume
         for ticker in self.tickers:
 
             close = f"{ticker}_close"
@@ -71,22 +95,12 @@ class Alpaca:
             ticker = self.df_portfolio[ticker][['close', 'volume']]
             
             clean_ticker_df.append(ticker)
-            
 
-        # for df in clean_ticker_df:
-        #     print(df)
-
+        # Concat resulting dataframes into one main dataframe
         self.clean_df = pd.concat(clean_ticker_df, axis='columns', join='inner')
         self.clean_df.columns = columns
 
-        # print(columns)
-        # print(self.clean_df)
-
         return self.clean_df
-
-#    def sample_df(self):
-#        self.clean_df.head()
-#        self.clean_df.tail()
 
     def run(self):
 
@@ -100,7 +114,6 @@ class Alpaca:
 
         self.clean_dataframe()
 
-    #    self.sample_df()
         print(self.clean_df)
         
         return self.clean_df
